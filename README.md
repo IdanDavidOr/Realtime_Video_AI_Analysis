ab89979c551f0e73b91097ce42435bfaf9b02208
Idan Lavi <idaneurosc@gmail.com>
2023-10-04 01:33:41 +0300

    running stream casting yoloV5 over video

---
+++ README.md
@@ -0,0 +1,19 @@
+# Real-Time Video Analysis Pipeline – Captain's Eye
+## Submission by: Idan Lavi
+
+## Installation - anaconda is needed for this step
+
+Open anaconda terminal and navigate to the directory this file is in
+
+(Optional) New anaconda environment
+   1. Create a new env using `conda create --name myenv`
+
+   2. activate the new env using `conda activate myenv`
+
+you can change `myenv` to any name you'd like 
+
+To install the required modules run `conda install -c conda-forge --file requirements.txt`
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-import json
 import cv2
+import torch
 
-class RTVAP:
-    def __init__(self, url):
-        self.url = url
+# Load YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+def stream_rtsp(rtsp_url):
+    cap = cv2.VideoCapture(rtsp_url)
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Failed to receive frame.")
+            break
+
+        results = model(frame)
+
+        cv2.imshow('RTSP Stream', results.render()[0])
+        # cv2.imshow('RTSP Stream', frame)
+
+        # Press 'q' to exit the loop and stop streaming
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return results
+
+
+
+if __name__ == "__main__":
+    rtsp_url = 'https://5e0da72d486c5.streamlock.net:8443/ayalon/HaShalom.stream/playlist.m3u8'  # Replace with your RTSP URL
+    res = stream_rtsp(rtsp_url)
+
